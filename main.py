d7c4c6266eed1ab8a4c97ff3da9e3392ada66073
HiteshChugh-2006 <hiteshchugh101@gmail.com>
2026-06-09 18:13:36 +0530

    Fix metrics import and retrain model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import joblib
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from sklearn.datasets import fetch_openml
 
 from sklearn.model_selection import (
